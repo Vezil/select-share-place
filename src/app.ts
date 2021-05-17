89f647d255ba57dff7980b0f5a7446d0855b3ae4
Vezil <SzymonWojaczek@wp.pl>
2021-05-17 19:31:04 +0200

    dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
+//config
+
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+//project
+
 const form = document.querySelector('from')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+// const { GOOGLE_API_KEY } = process.env;
+
+//dotenv fix (fs)
 
 function searchAddressHandler(event: Event) {
     event.preventDefault();
