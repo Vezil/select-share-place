587ba10c144fc98dd3543b7794970b807097930a
Vezil <SzymonWojaczek@wp.pl>
2021-05-24 19:06:16 +0200

    catching response from google

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,38 @@
-const form = document.querySelector('from')!;
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
 
-console.log(process.env.GOOGLE_API_KEY);
+type GoogleGeocodingResponseData = {
+    results: { geometry: { location: { lat: number; lng: number } } }[];
+    status: 'OK' | 'ZERO_RESULTS';
+};
 
-function searchAddressHandler(event: Event) {
+async function searchAddressHandler(event: Event) {
     event.preventDefault();
 
     const enteredAddress = addressInput.value;
+    const address = encodeURI(enteredAddress);
 
-    console.log(enteredAddress);
+    try {
+        const { data } = await axios.get<GoogleGeocodingResponseData>(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_API_KEY}`
+        );
+
+        if (data.status !== 'OK') {
+            const errorMessage = 'Could not fetch location!';
+
+            alert(errorMessage);
+
+            console.error(errorMessage);
+        }
+
+        const coordinates = data.results[0].geometry.location;
+    } catch (error) {
+        alert('Something went wrong!');
+
+        console.error(error);
+    }
 }
 
 form.addEventListener('submit', searchAddressHandler);
