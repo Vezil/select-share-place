72020f26b6b368684ea82d0d288d34e25ccd02fb
Vezil <44679435+Vezil@users.noreply.github.com>
2021-05-24 19:31:03 +0200

    Feature/map with google maps (#1)
    * add prettier and some basic styling
    
    * getting user input
    
    * dotenv
    
    * implement dotenv
    
    * catching response from google
    
    * add google script
    
    * add package lock to git ignore

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,54 @@
-// Code goes here!
-// Code goes here!
+import axios from 'axios';
+
+const form = document.querySelector('form')!;
+const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+type GoogleGeocodingResponseData = {
+    results: { geometry: { location: { lat: number; lng: number } } }[];
+    status: 'OK' | 'ZERO_RESULTS';
+};
+
+// declare const google: any;
+
+async function searchAddressHandler(event: Event) {
+    event.preventDefault();
+
+    const enteredAddress = addressInput.value;
+    const address = encodeURI(enteredAddress);
+
+    try {
+        const { data } = await axios.get<GoogleGeocodingResponseData>(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_API_KEY}`
+        );
+
+        if (data.status !== 'OK') {
+            const errorMessage = 'Could not fetch location!';
+
+            alert(errorMessage);
+
+            console.error(errorMessage);
+        }
+
+        const coordinates = data.results[0].geometry.location;
+
+        const map = new google.maps.Map(document.getElementById('map')!, {
+            center: coordinates,
+            zoom: 8
+        });
+
+        new google.maps.Marker({ position: coordinates, map });
+    } catch (error) {
+        alert('Something went wrong!');
+
+        console.error(error);
+    }
+}
+
+function appendGoogleScript() {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}`;
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+appendGoogleScript();
+form.addEventListener('submit', searchAddressHandler);
